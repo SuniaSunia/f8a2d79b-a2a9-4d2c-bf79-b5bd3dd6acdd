bcae43f74d5274d2f2570122c09ac13617ae44d2
Sunia <sunia1711@gmail.com>
2025-03-05 22:41:33 -0500

    modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');  // To resolve the output path
 
 module.exports = {
+  mode: 'production', // Ensure the build is in production mode
+  entry: './src/index.js', // Make sure this points to your main entry point
+  output: {
+    path: path.resolve(__dirname, 'build'), // The output folder where your build files will go
+    filename: 'bundle.js', // The name of the main JS bundle
+    publicPath: '/', // Ensure assets are correctly linked for production
+  },
   module: {
     rules: [
       {
@@ -8,27 +16,34 @@ module.exports = {
         use: [
           {
             loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
+            options: { minimize: true }, // Minify the HTML for production
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ['style-loader', 'css-loader'], // Ensure CSS is bundled
       },
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/, // Babel loader for JavaScript and JSX files
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      template: './public/index.html', // Template for the main HTML file
+      filename: './index.html', // Output filename
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all', // Split chunks for better caching
+    },
+    minimize: true, // Enable minimization for production builds
+  },
+  devtool: 'source-map', // Enable source maps for easier debugging (optional)
 };
